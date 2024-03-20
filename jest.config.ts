ef7655587b9ec5453e1dc9b41e8ad184529ff15c
Rhys Armour <rhys.armour@sainsburysbank.co.uk>
2024-03-20 17:25:14 +0000

    updated axios to node-fetch

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,35 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  verbose: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  testMatch: [
+    '<rootDir>/__tests__/**/*.test.ts?(x)',
+    '<rootDir>/(test|src)/**/*(*.)@(spec|test).test.ts?(x)',
+    '<rootDir>/src/**/__tests__/**/*.ts?(x)',
+    '<rootDir>/(test|src)/**/*(*.)@(spec|test).ts?(x)',
+  ],
+  coveragePathIgnorePatterns: ['<rootDir>/__tests__/mocks/*', '<rootDir>/src/config/params.ts'],
+  testPathIgnorePatterns: ['node_modules/*'],
   clearMocks: true,
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testNamePattern: '.test.ts',
+  collectCoverage: true,
+  coverageReporters: ['json', 'text'],
+  coverageDirectory: 'coverage',
+  watchPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '^.+\\.[t]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
+  },
 };
 
 export default config;
--- test/unit/validator.test.ts
+++ test/unit/validator.test.ts
@@ -4,10 +4,9 @@ jest.mock('axios');
 
 describe('validatorService', () => {
   it('should validate a valid Jira ticket URL', async () => {
-    const jiraTicketURL = 'https://jira.example.com/browse/ISSUE-12345';
-    const isValid = await validateJiraTicket(jiraTicketURL);
+    const jiraTicketURL = 'RFC-65855';
+    const isValid = await validateJiraTicket(jiraTicketURL).then((response) => expect(response).toBe(201));
 
-    expect(isValid).toBe(true);
-  }
-  )}
-)
+    // expect(isValid).toBe(true);
+  });
+});
