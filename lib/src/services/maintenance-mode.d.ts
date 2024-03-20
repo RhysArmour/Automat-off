25bc8ea4ab6b54218736ccb1ffcae050ba0c7ee3
Rhys Armour <rhys.armour@sainsburysbank.co.uk>
2024-03-20 17:54:15 +0000

    fixed entry points and start script

---
+++ lib/src/services/maintenance-mode.d.ts
@@ -0,0 +1,11 @@
+export declare const getMobileMaintenanceStatus: () => Promise<{
+    status: number;
+    message: string;
+    outageState: string;
+}>;
+export declare const getWebMaintenanceStatus: () => Promise<{
+    status: number;
+    message: string;
+    outageState: string;
+}>;
+//# sourceMappingURL=maintenance-mode.d.ts.map
